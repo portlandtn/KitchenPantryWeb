83fa7c6de5a523e6071363cd39f4f1819517196e
Jedidiah May <jedmay83@gmail.com>
2020-02-04 18:07:10 -0600

    Add project files.

---
+++ KitchenPantryWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KitchenPantryWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
